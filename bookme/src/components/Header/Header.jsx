964ee3ab1a21f8d2ccc8adfd5e80513845163289
Abdulrehman <a.rehmann616@gmail.com>
2023-11-27 19:48:51 +0500

    second commit

---
+++ bookme/src/components/Header/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from "styled-components";
+
+function Header() {
+  const Header = styled.header`
+     width: 100%;
+     border-bottom: 1px solid gainsboro;
+     div{
+      background-color: royalblue;
+      color: white;
+      padding: 5px;
+      text-align: center;
+      i{
+        color: yellow;
+        font-size: 20px;
+        margin-right: 5px;
+      }
+      a{
+           text-decoration: none;
+           border-bottom: 1px solid white;
+           color: white;
+           font-family: 600;
+      }
+     }
+
+  `
+  return (
+    <Header>
+       <div>
+       <i class="fa-solid fa-chess-queen"></i>
+         Become a member of <strong>BookmeBro</strong> and get amazing discounts and exclusive perks. <a href="#">Subscribe Now</a>
+       </div>
+    </Header>
+  )
+}
+
+export default Header
